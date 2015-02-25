7701d37651b8f63f65b5e3bc3ace873dcc64cad1
Daniel-Davis <ddv9461@saintjoe.edu>
2015-02-25 11:47:19 -0600

    Update item.js

--- item.js
+++ item.js
@@ -1,3 +1,8 @@
 // Daniel Davis, item, any object that can be used//
 
-var item = {};// and obejct that can be used
+var item = { // empty constructor for an item
+  var name = ''; // empty name
+  var consumable = false; // default setting
+  var equipable = false; // default setting
+  
+};// end of item
